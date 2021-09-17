143f16b64e3ac48e51f27df3a56436462c8197d5
merobi-hub <merobinson@uri.edu>
2021-09-17 00:10:53 -0400

    config edit

--- config.py
+++ config.py
@@ -5,9 +5,9 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config():
 
-    # SECRET_KEY = os.environ.get('SECRET KEY') or 'You will never guess...'
-    # SQLALCHEMY_DATABASE_URI = os.environ.get('DEPLOY_DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
-    # SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.environ.get('SECRET KEY') or 'You will never guess...'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    DATABASE_URL = os.environ.get('DATABASE_URL')
-    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+    # DATABASE_URL = os.environ.get('DATABASE_URL')
+    # conn = psycopg2.connect(DATABASE_URL, sslmode='require')
