7838675d6900779cb61c8ed0d374e5bc104785a8
merobi-hub <merobinson@uri.edu>
2021-09-17 00:24:13 -0400

    config edit

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config():
 
     SECRET_KEY = os.environ.get('SECRET KEY') or 'You will never guess...'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEPLOY_DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # DATABASE_URL = os.environ.get('DATABASE_URL')
