d58711488232a5ffc2c5705bc1f1ba13fd8d0f4b
merobi-hub <merobinson@uri.edu>
2021-10-31 13:36:45 -0400

    added percentage calculation to calculateall route

--- attendance/site/routes.py
+++ attendance/site/routes.py
@@ -220,7 +220,7 @@ def calculateAll():
         else:
             total_attendance[p.first_name.strip() + ' ' + p.last_name.strip()] = 1
     for k, v in total_attendance.items():
-        v = [v, (v[0]*100)/total_events]
+        v = [v, (v*100)/total_events]
         # v.append( (v[0]*100)/total_events )
 
     print(total_attendance)
