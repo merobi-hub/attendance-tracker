9e6895088d2101bb08d19b6e3e9206759a13cb1b
merobi-hub <merobi@gmail.com>
2025-04-21 09:24:13 -0400

    Fix in config
    Signed-off-by: merobi-hub <merobi@gmail.com>

--- config.py
+++ config.py
@@ -5,15 +5,15 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config():
 
-    SECRET_KEY = os.environ.get('SECRET KEY') or 'You will never guess...'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DEPLOY_DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SECRET_KEY = os.environ['SECRET KEY'] or 'You will never guess...'
+    SQLALCHEMY_DATABASE_URI = os.environ['DEPLOY_DATABASE_URL'] or 'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    GOOGLE_CLIENT_ID = os.environ.get('GOOGLE_CLIENT_ID', None)
-    GOOGLE_CLIENT_SECRET = os.environ.get('GOOGLE_CLIENT_SECRET', None)
+    GOOGLE_CLIENT_ID = os.environ['GOOGLE_CLIENT_ID', None]
+    GOOGLE_CLIENT_SECRET = os.environ['GOOGLE_CLIENT_SECRET', None]
     GOOGLE_DISCOVERY_URL = (
         "https://accounts.google.com/.well-known/openid-configuration"
     )
-    GOOGLE_REDIRECT_URI = os.environ.get('GOOGLE_REDIRECT_URI', None)    
+    GOOGLE_REDIRECT_URI = os.environ['GOOGLE_REDIRECT_URI', None]    
 
-    # DATABASE_URL = os.environ.get('DATABASE_URL')
-    # conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+    DATABASE_URL = os.environ['DATABASE_URL']
+    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
